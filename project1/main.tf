a81075bdd955af5edddfe19829d2be86f2768c59
mayurguptaiiitm <mayurgupta.iiitm@gmail.com>
2022-11-20 15:39:36 -0600

    adding webserver

--- project1/main.tf
+++ project1/main.tf
@@ -14,8 +14,28 @@ terraform {
 resource "aws_instance" "example" {
   ami = "ami-08c40ec9ead489470" #ubuntu 22.04 LTS
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+
+#   user_data_replace_on_change = true
 
   tags = {
     "Name" = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
