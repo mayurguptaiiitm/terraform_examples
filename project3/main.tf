ee6da99ee87df0835e5801b92666d15016d6d727
mayurguptaiiitm <mayurgupta.iiitm@gmail.com>
2023-02-13 00:02:51 -0600

    pg created second provider integration in progress

--- project3/main.tf
+++ project3/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.18.0"
+    }
   }
 }
 
@@ -16,13 +20,17 @@ provider "azurerm" {
     client_secret = var.secret
     skip_provider_registration = true
 }
+# reading from RG
+data "azurerm_resource_group" "labrg" {
+  name = var.resource_group_name
 
+}
 # adding postgresql database
 
 resource "azurerm_postgresql_server" "labpostgres" {
   name = var.postgresql_server_name
   location = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.labrg.name
 
   sku_name = "B_Gen5_2"
 
@@ -35,4 +43,33 @@ resource "azurerm_postgresql_server" "labpostgres" {
   administrator_login_password = var.postgresql_admin_password
   version                      = "11"
   ssl_enforcement_enabled      = true
+}
+
+resource "azurerm_postgresql_database" "labpostgres" {
+  name = "testdb"
+  resource_group_name = data.azurerm_resource_group.labrg.name
+  server_name = azurerm_postgresql_server.labpostgres.name
+  charset = "UTF8"
+  collation = "English_United States.1252"
+}
+
+data "azurerm_postgresql_server" "labpostgres" {
+  name = var.postgresql_server_name
+  resource_group_name = data.azurerm_resource_group.labrg.name
+}
+
+provider "postgresql" {
+  host = data.azurerm_postgresql_server.labpostgres.fqdn
+  port = 5432
+  database = azurerm_postgresql_database.labpostgres.name
+  username = data.azurerm_postgresql_server.labpostgres.administrator_login@azurerm_postgresql_database.labpostgres.name
+  password = var.postgresql_admin_password
+  connect_timeout = 15
+  alias = "admindb"
+}
+
+resource "postgresql_role" "userrole" {
+  provider = postgresql.admindb
+
+  name = "user_role"
 }
--- project3/output.tf
+++ project3/output.tf
@@ -1,3 +1,26 @@
 # output "resource_group_name" {
 #   value = data.azurerm_resource_group.name.name
 # }
+
+output "postgresql_fqdn_name" {
+    value = data.azurerm_postgresql_server.labpostgres.fqdn
+}
+
+output "postgresql_server_name" {
+    value = data.azurerm_postgresql_server.labpostgres.name
+}
+
+output "postgresql_server_admin" {
+    value = data.azurerm_postgresql_server.labpostgres.administrator_login
+}
+
+output "postgresql_database_name" {
+    value = azurerm_postgresql_database.labpostgres.name
+  
+}
+
+output "postgresql_database_server_name" {
+    value = azurerm_postgresql_database.labpostgres.server_name
+  
+}
+
